d6ba80de8f302d1d507151bc681c567f9b5b023d
charanpreet01 <charanpreetsinghrajput@gmail.com>
2025-07-29 10:25:45 +0530

    login page added

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import styles from '../ui/login/login.module.css'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1 className={styles.title}>Login</h1>
+        <input type="text" placeholder='Username' />
+        <input type="password" placeholder='Password' />
+        <button type="submit">Login</button>
+      </form>
+    </div>
   )
 }
 
